c618a02466560c8e2e613e00f66132851c66250d
beevik <beevik@yahoo.com>
2013-06-05 22:49:04 -0700

    optimized ParseString and IsGuid and cut time in half.

--- guid.go
+++ guid.go
@@ -1,11 +1,10 @@
 // Package guid defines a type for globally unique identifiers.  It
-// provides functions to generate RFC4122-compliant guids, to parse
+// provides functions to generate RFC 4122-compliant guids, to parse
 // strings into guids, and to convert guids to strings.
 package guid
 
 import (
     "crypto/rand"
-    "encoding/hex"
     "errors"
     "fmt"
 )
@@ -21,26 +20,15 @@ func init() {
     ErrInvalid = errors.New("guid: invalid format")
 }
 
-// New generates a random RFC4122-conformant version 4 Guid.
+// New generates a random RFC 4122-conformant version 4 Guid.
 func New() *Guid {
     g := new(Guid)
     if _, err := rand.Read(g[:]); err != nil {
         panic(err)
     }
     g[6] = (g[6] & 0x0f) | 0x40 // version = 4
-    g[8] = (g[8] & 0x3f) | 0x80 // variant = RFC4122
+    g[8] = (g[8] & 0x3f) | 0x80 // variant = RFC 4122
     return g
-}
-
-// ParseString returns the Guid represented by the string s.
-func ParseString(s string) (*Guid, error) {
-    if !IsGuid(s) {
-        return nil, ErrInvalid
-    }
-    g := new(Guid)
-    b, _ := hex.DecodeString(s[0:8] + s[9:13] + s[14:18] + s[19:23] + s[24:36])
-    copy(g[:], b)
-    return g, nil
 }
 
 // IsGuid returns true if the string contains a properly formatted Guid.
@@ -51,15 +39,52 @@ func IsGuid(s string) bool {
     if s[8] != '-' || s[13] != '-' || s[18] != '-' || s[23] != '-' {
         return false
     }
-    stripped := [...]string{s[0:8], s[9:13], s[14:18], s[19:23], s[24:36]}
-    for _, sub := range stripped {
-        for _, c := range sub {
-            if (c < '0' || c > '9') && (c < 'a' || c > 'f') && (c < 'A' || c > 'F') {
+    for _, sub := range [...]string{s[0:8], s[9:13], s[14:18], s[19:23], s[24:36]} {
+        for i := 0; i < len(sub); i++ {
+            if hextodec(sub[i]) == 255 {
                 return false
             }
         }
     }
     return true
+}
+
+// ParseString returns the Guid represented by the string s.
+func ParseString(s string) (*Guid, error) {
+    if len(s) != 36 {
+        return nil, ErrInvalid
+    }
+    if s[8] != '-' || s[13] != '-' || s[18] != '-' || s[23] != '-' {
+        return nil, ErrInvalid
+    }
+    g := new(Guid)
+    offset := 0
+    for _, sub := range [...]string{s[0:8], s[9:13], s[14:18], s[19:23], s[24:36]} {
+        for i := 0; i < len(sub); i, offset = i+2, offset+1 {
+            c0 := hextodec(sub[i])
+            c1 := hextodec(sub[i+1])
+            if (c0 | c1) == 255 {
+                return nil, ErrInvalid
+            }
+            g[offset] = c0<<4 | c1
+        }
+    }
+    return g, nil
+}
+
+// hextodec is a helper function that converts a hexadecimal character to
+// its decimal value
+func hextodec(c byte) byte {
+    if c >= '0' && c <= '9' {
+        return c - '0'
+    }
+    if c >= 'a' && c <= 'f' {
+        return c - 'a' + 10
+    }
+    if c >= 'A' && c <= 'F' {
+        return c - 'A' + 10
+    }
+    return 255      // represents invalid character
 }
 
 // String returns a standard hexadecimal string version of the Guid.
@@ -74,7 +99,7 @@ func (g *Guid) StringUpper() string {
     return fmt.Sprintf("%X-%X-%X-%X-%X", g[0:4], g[4:6], g[6:8], g[8:10], g[10:16])
 }
 
-// IsConformant determines if the GUID is RFC-4122 conformant.  If the
+// IsConformant determines if the Guid is RFC 4122-conformant.  If the
 // variant is "reserved for future definition" or the version is unknown,
 // then it is non-conformant.
 func (g *Guid) IsConformant() bool {
--- guid_test.go
+++ guid_test.go
@@ -23,12 +23,15 @@ func TestParseString(t *testing.T) {
     var goodGuids = [...]string{
         "0e545c9c-f942-4988-4ab0-645274cfaded",
         "22e2c08b-82bd-449a-7fc7-6ff9558ba733",
-        "3D767098-98CC-42D3-41E4-B09177487D0C",
+        "3D7670ff-98CC-42D3-41E4-B09177487D0C",
         "33C69DB0-D895-4D6F-7128-1855D3995742",
     }
     for _, s := range goodGuids {
+        if !IsGuid(s) {
+            t.Errorf("good guid '%v' failed IsGuid test\n", s)
+        }
         if _, err := ParseString(s); err != nil {
-            t.Errorf("good guid '%v' failed to parse [%v]", s, err)
+            t.Errorf("good guid '%v' failed to parse [%v]\n", s, err)
         }
     }
     var badGuids = [...]string{
@@ -38,6 +41,9 @@ func TestParseString(t *testing.T) {
         "33C69DB0D8954D6F71281855D3995742",
     }
     for _, s := range badGuids {
+        if IsGuid(s) {
+            t.Error("bad guid passed IsGuid test")
+        }
         if _, err := ParseString(s); err != ErrInvalid {
             t.Error("bad guid parsed")
         }
@@ -62,5 +68,16 @@ func BenchmarkParseString(b *testing.B) {
     b.ResetTimer()
     for i := 0; i < b.N; i++ {
         ParseString(guids[i%16])
+    }
+}
+
+func BenchmarkIsGuid(b *testing.B) {
+    var guids [16]string
+    for i := 0; i < 16; i++ {
+        guids[i] = New().String()
+    }
+    b.ResetTimer()
+    for i := 0; i < b.N; i++ {
+        IsGuid(guids[i%16])
     }
 }
